2d95ba927a00c5c54426e372a94feca23c1e02f5
Levi Marvin <levimarvin2020@outlook.com>
2021-04-03 19:48:18 +0800

    Fix command register.

--- src/main/java/site/levimarvin/pxautomessage/minecraft/Register.java
+++
@@ -1,9 +0,0 @@
-package site.levimarvin.pxautomessage.minecraft;
-
-public class Register {
-    public static void register() {
-        //Register command.
-        Command.registerCmd();
-        //Register listener.
-    }
-}
