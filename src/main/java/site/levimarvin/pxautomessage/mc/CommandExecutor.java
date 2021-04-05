69243f3894442c01de4027c3e0c3339fb85d2da7
Levi Marvin <levimarvin2020@outlook.com>
2021-04-05 23:19:22 +0800

    Update CommandExecutor.java

--- src/main/java/site/levimarvin/pxautomessage/mc/CommandExecutor.java
+++ src/main/java/site/levimarvin/pxautomessage/mc/CommandExecutor.java
@@ -3,6 +3,7 @@ package site.levimarvin.pxautomessage.mc;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import site.levimarvin.pxautomessage.annotation.Permission;
+import site.levimarvin.pxautomessage.annotation.Commander;
 
 public class CommandExecutor implements org.bukkit.command.CommandExecutor {
     private final org.bukkit.command.CommandExecutor COMMAND_EXECUTOR;
@@ -14,8 +15,27 @@ public class CommandExecutor implements org.bukkit.command.CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Permission permission = COMMAND_EXECUTOR.getClass().getAnnotation(Permission.class);
+        Commander commander = COMMAND_EXECUTOR.getClass().getAnnotation(Commander.class);
         if (sender.hasPermission(permission.permission())) {
-            COMMAND_EXECUTOR.onCommand(sender, command, label, args);
+            //TODO: THERE MAYBE HAVE SOME BUGS!
+            switch (commander.only()) {
+                case "console":
+                    if (sender instanceof Player) {
+                        sender.sendMessage("Only the CONSOLE can execute this!");
+                        break;
+                    }
+                    COMMAND_EXECUTOR.onCommand(sender, command, label, args);
+                    break;
+                case "player":
+                    if (sender instanceof Player) {
+                        sender.sendMessage("Only the PLAYER can execute this!");
+                        break;
+                    }
+                    COMMAND_EXECUTOR.onCommand(sender, command, label, args);
+                    break;
+                default:
+                    COMMAND_EXECUTOR.onCommand(sender, command, label, args);
+            }
         } else {
             sender.sendMessage("§cYou have not permission to do this!");
         }
